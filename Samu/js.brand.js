59b05c8ac9137976924029032a5a291bcf296ef9
SamuAP19 <salvarezpineda5@gmail.com>
2025-12-11 17:46:16 +0100

    "changes"

--- Samu/js.brand.js
+++ Samu/js.brand.js
@@ -1,3 +1,20 @@
+document.addEventListener('DOMContentLoaded', () => {
+        const hamburger = document.querySelector(".hamburger");
+        const navMenu = document.querySelector(".nav-menu");
+
+        hamburger.addEventListener("click", () => {
+            hamburger.classList.toggle("active");
+            navMenu.classList.toggle("active");
+        });
+
+        document.querySelectorAll(".nav-menu a").forEach(link => {
+            link.addEventListener("click", () => {
+                hamburger.classList.remove("active");
+                navMenu.classList.remove("active");
+            });
+        });
+    });
+    
 const carousel = document.getElementById('brands-carousel');
 const arrowLeft = document.getElementById('arrow-left');
 const arrowRight = document.getElementById('arrow-right');
